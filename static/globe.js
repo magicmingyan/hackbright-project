484d6e3124330d4a22e75ef894f6c4032074488a
Ming Yan <mingyan.vt@gmail.com>
2018-12-03 21:07:51 -0800

    changed source from planetaryjs.min.js to planetaryjs.js

--- static/globe.js
+++ static/globe.js
@@ -1,24 +1,22 @@
 var planet = planetaryjs.planet();
-// You can remove this statement if `world-110m.json`
-// is in the same path as the HTML page:
+
 
 planet.loadPlugin(planetaryjs.plugins.earth({
   topojson: { file: 'static/world-110m.json' }
 }));
 // Make the planet fit well in its canvas
-planet.projection.scale(500).translate([500, 500]);
+planet.projection.scale(300).translate([300, 300]);
 var canvas = document.getElementById('globe');
 
 
 planetaryjs.plugins.zoom({
-  scaleExtent: [300, 1000],
+  scaleExtent: [300, 500],
   onZoom: function() {
     console.log("The planet was zoomed!", this, d3.event);
   }
 });
-
 planet.loadPlugin(planetaryjs.plugins.zoom({
-    scaleExtent: [300, 1000]
+    scaleExtent: [300, 500]
   }));
 
 planetaryjs.plugins.drag({
@@ -26,7 +24,29 @@ planetaryjs.plugins.drag({
     console.log("The planet was dragged!", this, d3.event);
   }
 });
-
 planet.loadPlugin(planetaryjs.plugins.drag());
 
+
+
+planet.loadPlugin(planetaryjs.plugins.pings());
+
+var colors = ['red', 'yellow', 'white', 'orange', 'green', 'cyan', 'pink'];
+setInterval(function() {
+  var lat = Math.random() * 170 - 85;
+  var lng = Math.random() * 360 - 180;
+  var color = colors[Math.floor(Math.random() * colors.length)];
+  var angle = Math.random() * 10;
+  planet.plugins.pings.add(lng, lat, { color: color, ttl: 2000, angle: angle });
+}, 250);
+
+// // planet.loadPlugin(planetaryjs.plugins.objects());
+// console.log(planet.plugins.pings.add)
+
+// console.log(planet.plugins.pings.add)
+
+// // planet.plugins.objects.add(-1.3167103, 50.6927176, { imagesrc:"static/img/polar.png" });
+
+
 planet.draw(canvas);
+
+
