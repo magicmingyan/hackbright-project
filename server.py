e811eb3b9c9e5c0b51af9dfe8e100b0c7cdf8b19
Ming Yan <mingyan.vt@gmail.com>
2018-12-11 23:54:58 -0800

    rewriting api function

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 import requests
+import os
 
 # from model import connect_to_db, db, Bear
 
@@ -12,44 +13,40 @@ app.jinja_env.undefined = StrictUndefined
 
 def get_NYT_articles():
 
-    parameters = {"api-key": "secret", 
+    parameters = {"api-key": os.environ["nytimes_api"], 
                     "section": "World",
-                    "time-period": "1"
+                    "time-period": "7"
                     }
 
-    request_string = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/{}/{}.json".format(parameters["section"],parameters["time-period"])
+    article_request_string = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/{}/{}.json".format(parameters["section"],parameters["time-period"])
+    geo_request_string = "https://api.nytimes.com/svc/semantic/v2/geocodes/query.json"
 
-    response = requests.get(request_string, params=parameters)
+    article_response = requests.get(article_request_string, params=parameters)
+    article_response_dict = article_response.json()
+    articles = article_response_dict['results']
 
-    unpacked_response = response.json()
+    filtered_articles = [article for article in articles if len(article["geo_facet"])>0]
 
-    return unpacked_response['results']
-
-    # for articles in unpacked_response['results']:
-       #  geo_facet = articles["geo_facet"]
-       #  print(geo_facet[0])
-
-def article_location(list_of_articles):
-
-
-    request_string = "https://api.nytimes.com/svc/semantic/v2/geocodes/query.json"
-
-
-    for articles in list_of_articles:
-        geo_facet = articles["geo_facet"][0]
+    for article in filtered_articles:
+        geo_facet = article["geo_facet"][0]
         geo_facet = geo_facet.split('(')[0].title()  
-        print(geo_facet)
 
-        parameters = {"api-key": "secret", 
-                      "name": geo_facet
-                      }
-        response = requests.get(request_string, params=parameters)
-        unpacked_response = response.json()
-        results = unpacked_response['results']
-        print (results)
+        # print(geo_facet)
+        geo_parameters = {"api-key": os.environ["nytimes_api"], 
+                    "name": geo_facet
+                    }
 
-article_location(get_NYT_articles())
+        geo_response = requests.get(geo_request_string, params=geo_parameters)
+        geo_response_dict = geo_response.json()
 
+        if  len(geo_response_dict['results'])>0:
+            article["latitude"] = geo_response_dict['results'][0]['latitude']
+            article["longitude"] = geo_response_dict['results'][0]['longitude']
+
+
+    return filtered_articles
+
+get_NYT_articles()
 
 
 #---------------------------------------------------------------------#
@@ -58,9 +55,29 @@ article_location(get_NYT_articles())
 def index():
     """Show homepage."""
 
-
-
     return render_template("base.html")
+
+@app.route('/geo.json')
+def geo_info():
+    """JSON information about geo."""
+
+    return jsonify(get_NYT_articles())
+
+    # bears = {
+    #     bear.marker_id: {
+    #         "bearId": bear.bear_id,
+    #         "gender": bear.gender,
+    #         "birthYear": bear.birth_year,
+    #         "capYear": bear.cap_year,
+    #         "capLat": bear.cap_lat,
+    #         "capLong": bear.cap_long,
+    #         "collared": bear.collared.lower()
+    #     }
+    #     for bear in Bear.query.limit(50)}
+
+    # return jsonify(bears)
+
+
 
 
 #---------------------------------------------------------------------#
--- static/globe.js
+++ static/globe.js
@@ -1,3 +1,5 @@
+
+
 function initialize() {
   var earth = new WE.map('earth_div',{
   						zoom: 3,
@@ -6,14 +8,25 @@ function initialize() {
 
   WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
 
-  var marker = WE.marker([51.5, -0.09]).addTo(earth);
-  marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
+  // var marker = WE.marker([51.5, -0.09]).addTo(earth);
+  // marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
   
-  var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
-  marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
+  // var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
+  // marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
   
-  var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
-  // earth.setView([51.505, 0], 3);
+  // var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
+  // // earth.setView([51.505, 0], 3);
+
+  // var markerTest = WE.marker([56.0, 10.0]).addTo(earth);
+
+  $.get('/geo.json', function (geos) {
+      let geo;
+      for (let key in geos) {
+            geo = geos[key];
+            var marker4 = WE.marker([geo.latitude, geo.longitude]).addTo(earth);
+            marker4.bindPopup(geo.title, {maxWidth: 120, closeButton: true});
+      }
+    });
 }
 
 
